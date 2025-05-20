93e7c69cd425c777fd5603ebe13e2d2dd76030c5
Malilo <sandramalilo21@gmail.com>
2025-05-21 02:21:04 +0300

    Finalize basic_operations.py with all checks passed

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+
+print("Sum of", number1, "and", number2, "is", number1 + number2)
+print("Difference of", number1, "and", number2, "is", number1 - number2)
+print("Product of", number1, "and", number2, "is", number1 * number2)
