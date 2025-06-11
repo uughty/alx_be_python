1f824bb5e24f2cd9d6cbff2eaef4c9a0e1534e70
Malilo <sandramalilo21@gmail.com>
2025-06-11 13:35:01 +0300

    Fix: update display_balance to match required output format

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,20 +8,17 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self._account_balance += amount
-            return True
-        else:
-            print("Deposit Must be positive amount.")
-            return False
+           
+      
     
     def withdraw(self, amount):
-        if amount <= self._account_balance:
+        if 0 < amount <= self._account_balance:
             self._account_balance -= amount
             return True
-        else:
-            print("Insufficient funds.")
-            return False
+        return False
         
     def display_balance(self):
-        print(f"Current balnce:{self._account_balance:.2f}")
+      print(f"Current Balance: ${self._account_balance}")
+
         
     
