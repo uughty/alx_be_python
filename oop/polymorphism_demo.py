2207f64fbfe49b1060b3483c9d9e41e7a74d76e0
Malilo <sandramalilo21@gmail.com>
2025-06-18 01:32:16 +0300

    Implemented changes to calculate radius

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -19,4 +19,4 @@ class Circle(Shape):
         super().__init__()
         self.radius = radius
     def area(self):
-        return math.pi * self.radius **2
+        return math.pi * self.radius ** 2
