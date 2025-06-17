0e4122a583ff8147860d131286e15f028fcf8692
Malilo <sandramalilo21@gmail.com>
2025-06-18 00:57:30 +0300

    Added chnages

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,13 +1,14 @@
 class Book:
-    def __init__(self,title,author):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
     def __str__(self):
         return f"Book: '{self.title}' by {self.author}"
 
+
 class EBook(Book):
-    def __init__(self, title, author,file_size):
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
@@ -15,30 +16,23 @@ class EBook(Book):
         return f"EBook: '{self.title}' by {self.author}, File Size: {self.file_size}MB"
 
 
-
 class PrintBook(Book):
-    def __init__(self, title, author,page_count):
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
         return f"PrintBook: '{self.title}' by {self.author}, Pages: {self.page_count}"
 
+
 class Library:
     def __init__(self):
-        self.books =[]
-    def add_book(self,book):
-        if isinstance(book,Book):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
             self.books.append(book)
-    
+
     def list_books(self):
         for book in self.books:
-            if isinstance(book,EBook):
-                print(f"EBook: '{book.title}' by {book.author}, File Size: {book.file_size}MB")
-
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: '{book.title}' by {book.author}, Pages: {book.page_count}")
-            else:
-                print(f"Book: '{book.title}' by {book.author}")
-
-
+            print(book)  # LET __str__ do its magic
