043523b2977d07e81276c4da2c9772153c7e61b4
Malilo <sandramalilo21@gmail.com>
2025-06-18 12:23:59 +0300

    Implemented change in class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,14 +1,17 @@
 class Calculator:
-   calculator_type = "Arithmetic Operations"
+   calculation_type = "Arithmetic Operations"
 
-   @staticmethod
+   @staticmethod 
+
    def add(a,b):
-      return a +b
-   @classmethod
+      return a + b
+   
+   @classmethod 
+
    def multiply(cls,a,b):
       
-    print(f"Calculation type: {cls.calculator_type}")
-    return a*b 
+    print(f"Calculation type: {cls.calculation_type}")
+    return a * b 
    
 print("Sum:", Calculator.add(3, 7))             
 print("Product:", Calculator.multiply(4, 5))  
